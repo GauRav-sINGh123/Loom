0be6c704d312434dc5d522bc2d5eb0e924500ccf
Gaurav Singh <gksingh947@gmail.com>
2025-01-29 14:27:50 +0530

    User Model Recreated

---
+++ backend/models/user.model.js
@@ -0,0 +1,45 @@
+import {model, Schema} from "mongoose";
+
+const userSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    avatar:{
+        type:String
+    },
+    bannerImage:{
+        type:String
+    },
+    bio:{
+        type:String
+    },
+    location:{
+        type:String
+    },
+    connections:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"User"
+        }
+    ]
+
+},{timestamps:true});
+
+const User = model("User", userSchema);
+
+export default User

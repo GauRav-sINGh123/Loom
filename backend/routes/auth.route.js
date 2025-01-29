4a78b19c40ab1218b8f6603c969711fa3e5b1be6
Gaurav Singh <gksingh947@gmail.com>
2025-01-29 12:57:44 +0530

    Code Restructring

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,10 @@
-import express from "express";
-const router = express.Router();
-import { signup,signin } from "../controllers/auth.controller.js";
+// import express from "express";
+// const router = express.Router();
+// import { signup,signin } from "../controllers/auth.controller.js";
 
-router.route('/signup').post(signup);
+// router.route('/signup').post(signup);
 
-router.route('/signin').post(signin);
+// router.route('/signin').post(signin);
 
-router.route('/logout').post();
-export default router;
+// router.route('/logout').post();
+// export default router;

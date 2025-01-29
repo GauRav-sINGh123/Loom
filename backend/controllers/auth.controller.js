52ecd69557f23cd77fe982f52ed47cfe26325134
Gaurav Singh <gksingh947@gmail.com>
2025-01-29 15:08:42 +0530

    Signup Method Created

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,43 @@
+import {asyncHandler} from "../utils/asyncHandler.js";
+import User from "../models/user.model.js";
+
+export const signin=asyncHandler(async(req,res)=>{})
+
+export const signup = asyncHandler(async (req, res) => {
+    const { username, email, password, name} = req.body;
+
+    // Check for missing fields
+    if ([email, name, password,username].some((field) => !field?.trim())) {
+        return res.status(400).json({ success: false, message: "All fields are required" });
+    }
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        return res.status(409).json({ success: false, message: "User already exists" });
+    }
+    
+    // Hash password
+    if(password<8){ 
+     return res.status(400).json({ success: false, message: "Password must be at least 8 characters long" });
+    }
+      
+    
+    // Creates a new user
+    const user = await User.create({ username, email, password, name });
+
+    if (!user) {
+        return res.status(500).json({ success: false, message: "User not created" });
+    }
+
+     
+    return res.status(201).json({
+        success: true,
+        username,
+        email,
+        name
+    });
+});
+
+
+export const logout=asyncHandler(async(req,res)=>{})

25685ad737a427ca631cf63e19adb95050f6b865
Gaurav Singh <gksingh947@gmail.com>
2025-01-29 19:08:32 +0530

    User Logout Added in AuthRoute

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { signup,signin } from "../controllers/auth.controller.js";
+import { signup,signin,logout} from "../controllers/auth.controller.js";
 
 router.route('/signup').post(signup);
 
 router.route('/signin').post(signin);
 
-router.route('/logout').post();
+router.route('/logout').post(logout);
 export default router;

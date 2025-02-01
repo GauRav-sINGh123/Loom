82fee4c219806ea80f5d57fb10964457c98e6eca
Gaurav Singh <gksingh947@gmail.com>
2025-02-01 15:12:00 +0530

    Create Post Method Created

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,6 +1,6 @@
 import Post from "../models/post.model.js"
 import { asyncHandler } from "../utils/asyncHandler";
-
+import cloudinary from "../utils/cloudinary.js";
 
 
 export const getAllPosts=asyncHandler(async(req,res)=>{
@@ -16,3 +16,31 @@ export const getAllPosts=asyncHandler(async(req,res)=>{
     }
     res.status(200).json(posts)
 })
+
+export const createPost=async(req,res)=>{
+   try {
+    const {content,image}=req.body
+     
+    let newPost;
+ 
+    if(image){
+     const result= await cloudinary.uploader.upload(image)
+     newPost= await Post({
+         author:req.user._id,
+         content,
+         image:result.secure_url
+     })
+    }else{
+      newPost= new Post({
+         author:req.user._id,
+         content,
+      })
+    }
+    
+    await newPost.save()
+
+    res.status(201).json(newPost)
+   } catch (error) {
+     res.status(500).json("Failed To Create Post")
+   }
+}
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,7 +1,8 @@
 import express from "express"
 const router=express.Router()
 import {isProtected} from '../middlewares/auth.middleware'
+import { getAllPosts,createPost } from "../controllers/post.controller"
 
 router.route('/posts').get(isProtected,getAllPosts)
-router.route('/post').post()
+router.route('/post').post(isProtected,createPost)
 export default router

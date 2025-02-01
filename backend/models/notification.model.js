a896aab53e4950f4cf88033b951ab1de29088ff6
Gaurav Singh <gksingh947@gmail.com>
2025-02-01 12:03:23 +0530

    Notification Model Created

---
+++ backend/models/notification.model.js
@@ -0,0 +1,33 @@
+import {Schema,model} from "mongoose";
+
+const notificationSchema = new Schema(
+	{
+		recipient: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		type: {
+			type: String,
+			required: true,
+			enum: ["like", "comment", "requestAccepted"],
+		},
+		relatedUser: {
+			type:Schema.Types.ObjectId,
+			ref: "User",
+		},
+		relatedPost: {
+			type: Schema.Types.ObjectId,
+			ref: "Post",
+		},
+		read: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ timestamps: true }
+);
+
+const Notification = model("Notification", notificationSchema);
+
+export default Notification;

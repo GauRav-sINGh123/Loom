a078291994fb641e901145c40637dda087095b5d
Gaurav Singh <gksingh947@gmail.com>
2025-02-01 15:26:04 +0530

    Delete Post Method Created

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -2,7 +2,7 @@ import Post from "../models/post.model.js"
 import { asyncHandler } from "../utils/asyncHandler";
 import cloudinary from "../utils/cloudinary.js";
 
-
+// Method to get all posts
 export const getAllPosts=asyncHandler(async(req,res)=>{
     const posts=await Post.find({author:{$in:req.user.connections}})
     .populate("author", "name username profilePicture bio")
@@ -17,12 +17,14 @@ export const getAllPosts=asyncHandler(async(req,res)=>{
     res.status(200).json(posts)
 })
 
+// Method to create a post
 export const createPost=async(req,res)=>{
    try {
     const {content,image}=req.body
      
     let newPost;
- 
+  
+    //Uploads image to cloudinary if it exists
     if(image){
      const result= await cloudinary.uploader.upload(image)
      newPost= await Post({
@@ -30,13 +32,13 @@ export const createPost=async(req,res)=>{
          content,
          image:result.secure_url
      })
-    }else{
+    }else{ //Creates a post without an image
       newPost= new Post({
          author:req.user._id,
          content,
       })
     }
-    
+
     await newPost.save()
 
     res.status(201).json(newPost)
@@ -44,3 +46,32 @@ export const createPost=async(req,res)=>{
      res.status(500).json("Failed To Create Post")
    }
 }
+
+// Method to delete a post
+export const deletePost=asyncHandler(async(req,res)=>{
+    const postId=req.params
+    const userId=req.user._id
+    
+    const post=await Post.findById(postId)
+
+    if(!post){
+        return res.status(404).json({
+            message:"Post Not Found"
+        })
+    }
+   
+    //Checks if current user is the owner or author of the post 
+    if(post.author.toString() !== userId){
+        return res.status(403).status("Unauthorized")
+    }
+   
+    //Deletes the image from cloudinary if it exists
+    if(post.image){
+        await cloudinary.uploader.destroy(post.image.split("/").pop().split(".")[0])
+    }
+    await Post.findByIdAndDelete(postId)
+
+    res.status(200).json({
+        message:"Post Deleted"
+    })
+})
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,8 +1,11 @@
 import express from "express"
 const router=express.Router()
 import {isProtected} from '../middlewares/auth.middleware'
-import { getAllPosts,createPost } from "../controllers/post.controller"
+import { getAllPosts,createPost,deletePost} from "../controllers/post.controller"
 
 router.route('/posts').get(isProtected,getAllPosts)
+
 router.route('/post').post(isProtected,createPost)
+
+router.route("/post/:postId").delete(isProtected,deletePost)
 export default router
